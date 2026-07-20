bc5436b5463f7c6e94ff2b1d92179aaaae30ceaa
Sasha Lohozinskyy <0x0sky@proton.me>
2026-07-20 23:37:48 +0300

    Expand locale resolution by language family

--- lib/zero_x_da/market_client_bot/locale.rb
+++ lib/zero_x_da/market_client_bot/locale.rb
@@ -5,16 +5,36 @@ module ZeroXDA
     module Locale
       DEFAULT = "en_US"
       UKRAINIAN = "uk_UA"
-      SUPPORTED = [DEFAULT, UKRAINIAN].freeze
+      RUSSIAN = "ru_RU"
+      FRENCH = "fr_FR"
+      SPANISH = "es_ES"
+      GERMAN = "de_DE"
+
+      LANGUAGE_MAP = {
+        "en" => DEFAULT,
+        "uk" => UKRAINIAN,
+        "ru" => RUSSIAN,
+        "fr" => FRENCH,
+        "es" => SPANISH,
+        "de" => GERMAN
+      }.freeze
+      SUPPORTED = LANGUAGE_MAP.values.uniq.freeze
 
       module_function
 
       def resolve(language_code)
-        language_code.to_s.downcase.start_with?("uk") ? UKRAINIAN : DEFAULT
+        LANGUAGE_MAP.fetch(language_subtag(language_code), DEFAULT)
       end
 
       def normalize(locale)
-        SUPPORTED.include?(locale.to_s) ? locale.to_s : DEFAULT
+        value = locale.to_s
+        return value if SUPPORTED.include?(value)
+
+        resolve(value)
+      end
+
+      def language_subtag(value)
+        value.to_s.strip.downcase.split(/[-_]/, 2).first
       end
     end
   end
