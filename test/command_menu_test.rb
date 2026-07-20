cddcab646cc12da6117d2258ba5f13638157d735
Sasha Lohozinskyy <0x0sky@proton.me>
2026-07-20 21:21:53 +0300

    Cover localized command menu integration

--- test/command_menu_test.rb
+++ test/command_menu_test.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "test_helper"
+require "zero_x_da/market_client_bot/bot"
 require "zero_x_da/market_client_bot/command_menu"
 
 class CommandMenuTest < Minitest::Test
@@ -30,5 +31,35 @@ class CommandMenuTest < Minitest::Test
     commands = CommandMenu.client(locale: "uk_UA").map { |item| item.fetch(:command) }
 
     assert_equal %w[buy status], commands
+  end
+
+  def test_bot_syncs_ukrainian_admin_menu_from_persisted_locale
+    market = FakeMarketAPI.new
+    telegram = FakeTelegramAPI.new
+    bot = ZeroXDA::MarketClientBot::Bot.new(
+      market_api: market,
+      telegram_api: telegram,
+      status_message_ttl: 0
+    )
+    market.define_singleton_method(:authenticate_telegram) do |user:, chat:|
+      {
+        "id" => "admin-id",
+        "attributes" => { "role" => "admin", "status" => "active", "locale" => "uk_UA" }
+      }
+    end
+
+    bot.handle(
+      "message" => {
+        "text" => "/status",
+        "from" => { "id" => 99, "language_code" => "uk" },
+        "chat" => { "id" => 990, "type" => "private" }
+      }
+    )
+
+    descriptions = telegram.command_sets.last.fetch(:commands).to_h do |item|
+      [item.fetch(:command), item.fetch(:description)]
+    end
+    assert_equal "застосувати ціни", descriptions.fetch("apply_prices")
+    assert_equal "встановити курс валюти", descriptions.fetch("set_rate")
   end
 end
