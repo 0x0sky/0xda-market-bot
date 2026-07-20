ee8f4c09ce6088eae8d95275676bb4929a8d5e77
Sasha Lohozinskyy <0x0sky@proton.me>
2026-07-20 21:34:47 +0300

    Test Telegram locale fallback for command menus

--- test/command_menu_test.rb
+++ test/command_menu_test.rb
@@ -33,7 +33,7 @@ class CommandMenuTest < Minitest::Test
     assert_equal %w[buy status], commands
   end
 
-  def test_bot_syncs_ukrainian_admin_menu_from_persisted_locale
+  def test_telegram_language_is_used_when_core_user_has_no_persisted_locale
     market = FakeMarketAPI.new
     telegram = FakeTelegramAPI.new
     bot = ZeroXDA::MarketClientBot::Bot.new(
@@ -41,25 +41,54 @@ class CommandMenuTest < Minitest::Test
       telegram_api: telegram,
       status_message_ttl: 0
     )
-    market.define_singleton_method(:authenticate_telegram) do |user:, chat:|
-      {
-        "id" => "admin-id",
-        "attributes" => { "role" => "admin", "status" => "active", "locale" => "uk_UA" }
-      }
-    end
 
-    bot.handle(
-      "message" => {
-        "text" => "/status",
-        "from" => { "id" => 99, "language_code" => "uk" },
-        "chat" => { "id" => 990, "type" => "private" }
-      }
-    )
+    bot.handle(update(language_code: "uk"))
 
-    descriptions = telegram.command_sets.last.fetch(:commands).to_h do |item|
-      [item.fetch(:command), item.fetch(:description)]
-    end
+    descriptions = command_descriptions(telegram)
     assert_equal "застосувати ціни", descriptions.fetch("apply_prices")
     assert_equal "встановити курс валюти", descriptions.fetch("set_rate")
+  end
+
+  def test_persisted_locale_has_priority_over_telegram_language
+    market = Class.new(FakeMarketAPI) do
+      def authenticate_telegram(user:, chat:)
+        super.tap { |entry| entry.fetch("attributes")["locale"] = "en_US" }
+      end
+    end.new
+    telegram = FakeTelegramAPI.new
+    bot = ZeroXDA::MarketClientBot::Bot.new(
+      market_api: market,
+      telegram_api: telegram,
+      status_message_ttl: 0
+    )
+
+    bot.handle(update(language_code: "uk"))
+
+    descriptions = command_descriptions(telegram)
+    assert_equal "apply prices", descriptions.fetch("apply_prices")
+    assert_equal "set exchange rate", descriptions.fetch("set_rate")
+  end
+
+  private
+
+  def update(language_code:)
+    {
+      "message" => {
+        "text" => "/status",
+        "from" => {
+          "id" => 99,
+          "username" => "zero",
+          "first_name" => "Sasha",
+          "language_code" => language_code
+        },
+        "chat" => { "id" => 990, "type" => "private" }
+      }
+    }
+  end
+
+  def command_descriptions(telegram)
+    telegram.command_sets.last.fetch(:commands).to_h do |item|
+      [item.fetch(:command), item.fetch(:description)]
+    end
   end
 end
